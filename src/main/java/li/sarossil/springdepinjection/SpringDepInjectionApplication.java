9ffbc1dbb22b73802a99c033b3d03c2ccbe1fc1a
Sam Rossilli <Sarossilli@gmail.com>
2022-01-01 13:13:01 -0700

    Initial commit

---
+++ src/main/java/li/sarossil/springdepinjection/SpringDepInjectionApplication.java
@@ -0,0 +1,13 @@
+package li.sarossil.springdepinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDepInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDepInjectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/li/sarossil/springdepinjection/SpringDepInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package li.sarossil.springdepinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDepInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
